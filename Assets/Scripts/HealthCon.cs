44b0f06c2b13641cea8a02a4b40f800ba899d6d8
Markus <mflens22@student.aau.dk>
2023-11-24 16:52:24 +0100

    Added death screen

--- Assets/Scripts/HealthCon.cs
+++ Assets/Scripts/HealthCon.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +12,22 @@ public class HealthCon : MonoBehaviour
     public float health = 100f;
     
     public Slider healthSlider;
-   
+
+    public TextMeshProUGUI Deathtext;
+
+    private void Start()
+    {
+        Deathtext.enabled = false;
+    }
+
     private void Update()
     {
         healthSlider.value = health;
+        if (healthSlider.value == 0)
+        {
+            Deathtext.enabled = true;
+            Time.timeScale = 0;
+        }
     }
     
     public void TakeDamage(float Damage)
